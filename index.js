e4af03423e0c33445c521d8be622f7a596e67b90
Ting Chung <tchung682@gmail.com>
2020-05-11 21:52:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ var prependKitten = function(name){
   return [name, ...kittens]
 }
 var removeLastKitten = function(){
-  array = kittens.slice(kittens.length -2)
+  array = kittens.slice(2)
   return array
 }
